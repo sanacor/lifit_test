6ce4dcf2aca6e2724b68d8fed12f1aa38565677c
sanha kang <ncsana@gmail.com>
2022-01-06 12:02:56 +0900

    init

---
+++ lifitPushTestAppUITests/lifitPushTestAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lifitPushTestAppUITestsLaunchTests.swift
+//  lifitPushTestAppUITests
+//
+//  Created by dosoon on 2022/01/03.
+//
+
+import XCTest
+
+class lifitPushTestAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
